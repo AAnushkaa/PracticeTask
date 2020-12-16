fa8bfe93f960d0b0816d08f74a3502b2bb992e9d
abhi640 <abhi6340@gmail.com>
2020-12-17 01:42:10 +0530

    new pics and router added

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,30 +12,35 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1
+          imageUrl: 'https://www.wallpaperup.com/uploads/wallpapers/2013/03/20/54255/6078201605b6f67eaf795b91215f2092.jpg',
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
+          imageUrl: 'https://c.wallhere.com/photos/46/e7/women_long_hair_leather_jackets_looking_at_viewer_hands_on_hips_painted_nails_dark_hair_Sonakshi_Sinha-39612.jpg!d',
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
+          imageUrl: 'https://img4.goodfon.com/wallpaper/nbig/0/76/gigi-hadid-dzhidzhi-khadid-model-shatenka-sidit-na-kryshe-po.jpg',
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://img4.goodfon.com/wallpaper/big/b/d2/martin-kuhn-anna-martin-kuhn-model-girl-brunette-long-hair-s.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://c.wallhere.com/photos/b4/26/leonardo_dicaprio_actor_face_beard-553963.jpg!d',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
