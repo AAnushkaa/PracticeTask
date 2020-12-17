edf72e9a7e4c60f0bc888cc1797e8ba53773e262
abhi640 <abhi6340@gmail.com>
2020-12-18 00:21:17 +0530

    Shop page added

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,32 +12,32 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://www.wallpaperup.com/uploads/wallpapers/2013/03/20/54255/6078201605b6f67eaf795b91215f2092.jpg',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
           linkUrl: 'hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://c.wallhere.com/photos/46/e7/women_long_hair_leather_jackets_looking_at_viewer_hands_on_hips_painted_nails_dark_hair_Sonakshi_Sinha-39612.jpg!d',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
           linkUrl: ''
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://img4.goodfon.com/wallpaper/nbig/0/76/gigi-hadid-dzhidzhi-khadid-model-shatenka-sidit-na-kryshe-po.jpg',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
           linkUrl: ''
         },
         {
           title: 'womens',
-          imageUrl: 'https://img4.goodfon.com/wallpaper/big/b/d2/martin-kuhn-anna-martin-kuhn-model-girl-brunette-long-hair-s.jpg',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
           linkUrl: ''
         },
         {
           title: 'mens',
-          imageUrl: 'https://c.wallhere.com/photos/b4/26/leonardo_dicaprio_actor_face_beard-553963.jpg!d',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
           linkUrl: ''
